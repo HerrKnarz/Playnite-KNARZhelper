83de24dc11c6848a2391d7ea996cd33bfad0e0e2
Alexander Heidn <40741234+HerrKnarz@users.noreply.github.com>
2025-10-18 15:00:02 +0200

    initial commit

---
+++ KNARZhelper/MetadataCommon/Enum/ItemValueType.cs
@@ -0,0 +1,18 @@
+﻿namespace KNARZhelper.MetadataCommon.Enum
+{
+    /// <summary>
+    /// All supported item value types.
+    /// </summary>
+    public enum ItemValueType
+    {
+        None,
+        ItemList,
+        Integer,
+        String,
+        Date,
+        Media,
+        Boolean,
+        Ulong,
+        LinkList
+    }
+}
