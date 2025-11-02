ada5e823ac6bb6e40b69190f948bd5f171f39412
Alexander Heidn <40741234+HerrKnarz@users.noreply.github.com>
2025-11-02 16:12:18 +0100

    Added method to delete orphaned files for a game.

--- KNARZhelper/ScreenshotsCommon/Models/ScreenshotGroups.cs
+++ KNARZhelper/ScreenshotsCommon/Models/ScreenshotGroups.cs
@@ -2,6 +2,7 @@
 using Playnite.SDK.Data;
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
@@ -15,6 +16,8 @@ namespace KNARZhelper.ScreenshotsCommon.Models
     /// </summary>
     public class ScreenshotGroups : ObservableCollection<ScreenshotGroup>
     {
+        private Guid _gameId;
+
         /// <summary>
         /// Creates a new instance of the ScreenshotGroups class.
         /// </summary>
@@ -29,6 +32,7 @@ namespace KNARZhelper.ScreenshotsCommon.Models
         /// <param name="gameId">Unique identifier for the game. This is the name of the sub folder in the base path</param>
         public ScreenshotGroups(string basePath, Guid gameId)
         {
+            _gameId = gameId;
             CreateGroupsFromFiles(basePath, gameId);
         }
 
@@ -46,6 +50,8 @@ namespace KNARZhelper.ScreenshotsCommon.Models
         /// <param name="createEmptyGroupOnError"></param>
         public void CreateGroupsFromFiles(string basePath, Guid gameId, bool createEmptyGroupOnError = true)
         {
+            _gameId = gameId;
+
             if (gameId == Guid.Empty)
             {
                 if (createEmptyGroupOnError)
@@ -91,6 +97,65 @@ namespace KNARZhelper.ScreenshotsCommon.Models
                     Log.Error(ex, $"Failed to load screenshots from file {file.FullName}");
                 }
             }
+        }
+
+        public bool DeleteOrphanedFiles()
+        {
+            var deleted = false;
+
+            if (_gameId == Guid.Empty)
+            {
+                return deleted;
+            }
+
+            try
+            {
+                var directoryInfo = ScreenshotHelper.GetDownloadPath(gameId: _gameId, createDir: false);
+
+                if (directoryInfo is null || !directoryInfo.Exists)
+                {
+                    return false;
+                }
+
+                var filesToKeep = new HashSet<string>();
+
+                foreach (var group in this)
+                {
+                    filesToKeep.Add(group.FileName);
+
+                    filesToKeep.UnionWith(group.Screenshots.Where(s => !string.IsNullOrEmpty(s.DownloadedThumbnailPath)).Select(s => s.DownloadedThumbnailPath));
+
+                    filesToKeep.UnionWith(group.Screenshots.Where(s => !string.IsNullOrEmpty(s.DownloadedPath)).Select(s => s.DownloadedPath));
+                }
+
+                var files = directoryInfo.EnumerateFiles("*", SearchOption.AllDirectories);
+
+                var result = false;
+
+                foreach (var file in files)
+                {
+                    if (filesToKeep == null || !filesToKeep.Any(f => file.FullName.Equals(f)))
+                    {
+                        file.Delete();
+
+                        result = true;
+                    }
+                }
+
+                if (result)
+                {
+                    // We wait a bit to ensure the file system has updated
+                    Task.Delay(TimeSpan.FromMilliseconds(100));
+                }
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+            }
+
+            return deleted;
         }
 
         /// <summary>
