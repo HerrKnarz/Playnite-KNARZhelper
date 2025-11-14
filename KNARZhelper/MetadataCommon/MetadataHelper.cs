b9440283328cefcef83a2957f9d5d521dab0718b
Alexander Heidn <40741234+HerrKnarz@users.noreply.github.com>
2025-11-14 09:55:31 +0100

    Added class for metadata related helper methods containing a method to get a specific link from a game matching a regex.

---
+++ KNARZhelper/MetadataCommon/MetadataHelper.cs
@@ -0,0 +1,16 @@
+﻿using Playnite.SDK.Models;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace KNARZhelper.MetadataCommon
+{
+    internal class MetadataHelper
+    {
+        public static Link GetLink(Game game, Regex urlMask)
+        {
+            return game == null || urlMask == null
+                ? null
+                : (game.Links?.FirstOrDefault(l => l.Url != null && urlMask.IsMatch(l.Url)));
+        }
+    }
+}
