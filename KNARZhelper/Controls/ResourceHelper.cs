5d0bdcf094b6baaba2fd18460c10289b0ee4c842
Alexander Heidn <40741234+HerrKnarz@users.noreply.github.com>
2026-05-27 17:17:22 +0200

    feat(common): Added style for a hidden icon button and a method to add my own custom icon font.

---
+++ KNARZhelper/Controls/ResourceHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+
+namespace KNARZhelper.Controls
+{
+    public static class ResourceHelper
+    {
+        public static void LoadIconFont(string pluginPath)
+        {
+            if (Application.Current == null || Application.Current.Resources.Contains("KNARZIconFont"))
+            {
+                return;
+            }
+
+            var fontFile = new FileInfo(Path.Combine(pluginPath, "Resources\\KNARZIconFont.ttf"));
+
+            if (!fontFile.Exists)
+            {
+                Log.Info($"Font not found in {fontFile.FullName}");
+                return;
+            }
+
+            var fontFamily = new FontFamily(new Uri(fontFile.FullName, UriKind.Absolute), "./#KNARZIconFont");
+
+            Application.Current.Resources.Add("KNARZIconFont", fontFamily);
+        }
+    }
+}
