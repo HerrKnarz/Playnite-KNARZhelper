accf367fc1fa60d6ed4d69374949bd991bfd6652
Alexander Heidn <40741234+HerrKnarz@users.noreply.github.com>
2026-07-31 17:34:40 +0200

    feat(helper): Added potential workaround for bot filters.

--- KNARZhelper/WebCommon/WebHelper.cs
+++ KNARZhelper/WebCommon/WebHelper.cs
@@ -1,8 +1,11 @@
-﻿using KNARZhelper.WebCommon.Models;
+﻿using KNARZhelper.FilesCommon;
+using KNARZhelper.WebCommon.Models;
+using Playnite.SDK;
 using System;
 using System.IO;
 using System.Net;
 using System.Text.RegularExpressions;
+using System.Threading;
 
 namespace KNARZhelper.WebCommon
 {
@@ -68,5 +71,54 @@ namespace KNARZhelper.WebCommon
         /// <returns>decoded title of the page</returns>
         public static string GetPageTitle(string htmlSource) =>
             WebUtility.HtmlDecode(Regex.Match(htmlSource, @"\<title\b[^>]*\>\s*(?<Title>[\s\S]*?)\</title\>", RegexOptions.IgnoreCase).Groups["Title"].Value.Trim());
+
+        /// <summary>
+        /// Loads a web page with a smart wait to bypass Cloudflare protection.
+        /// </summary>
+        /// <param name="webView">WebView instance to use for loading the page</param>
+        /// <param name="url">URL of the page to load</param>
+        /// <param name="debugMode">
+        /// If true, saves the last fetched HTML to a file for debugging purposes
+        /// </param>
+        /// <param name="pluginId">Plugin ID to determine the path for saving the debug file</param>
+        /// <returns>HTML source of the loaded page</returns>
+        public static string LoadPageWithSmartWait(IWebView webView, string url, bool debugMode = false, Guid pluginId = default)
+        {
+            var htmlSource = string.Empty;
+
+            webView.NavigateAndWait(url);
+
+            var maxRetries = 10; // 10 attempts of 1 second
+            var attempts = 0;
+
+            while (attempts < maxRetries)
+            {
+                htmlSource = webView.GetPageSource();
+
+                // Check if we passed the Cloudflare barrier. If the HTML DOES NOT contain the block
+                // messages, it means the page loaded!
+                if (!string.IsNullOrEmpty(htmlSource) &&
+                    !htmlSource.Contains("Just a moment...") &&
+                    !htmlSource.Contains("cf-browser-verification") &&
+                    !htmlSource.Contains("challenges.cloudflare.com"))
+                {
+                    htmlSource = webView.GetPageSource();
+                    break;
+                }
+
+                // If still on the Cloudflare screen, wait 1 second and try reading the HTML again
+                Thread.Sleep(1000);
+                attempts++;
+            }
+
+            if (debugMode)
+            {
+                var basePath = API.Instance.Addons.Plugins.Find(p => p.Id == pluginId).GetPluginUserDataPath();
+
+                FileHelper.WriteStringToFile(Path.Combine(basePath, "last_fetched.html"), htmlSource);
+            }
+
+            return htmlSource;
+        }
     }
 }
